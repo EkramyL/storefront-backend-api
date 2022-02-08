ff2ccfb9865aa1785d15789c4afebd055f79790f
Ekramy Latief <ekramyl3000@gmail.com>
2022-02-08 20:33:14 +0200

    feat: Adding the product, order models

---
+++ src/models/order.ts
@@ -0,0 +1,62 @@
+import client from '../database';
+
+export type Order = {
+  id?: number;
+  status: string;
+  user_id: number;
+};
+
+export class OrderStore {
+  async index(): Promise<Order[]> {
+    try {
+      const conn = await client.connect();
+      const sql = 'SELECT * FROM orders';
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (error) {
+      throw new Error(`can't get orders. Error: ${error}`);
+    }
+  }
+
+  async show(id: number): Promise<Order> {
+    try {
+      const conn = await client.connect();
+      const sql = 'SELECT * FROM orders WHERE id=($1)';
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`unable to get orders . Error: ${error}`);
+    }
+  }
+
+  async create(o: Order): Promise<Order> {
+    try {
+      const conn = await client.connect();
+      const sql = 'INSERT INTO orders (status, user_id) VALUES ($1, $2)';
+      const result = await conn.query(sql, [o.status, o.user_id]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`unable to get orders . Error: ${error}`);
+    }
+  }
+
+  async addProduct(
+    quantity: number,
+    order_id: number,
+    product_id: number
+  ): Promise<Order> {
+    try {
+      const conn = await client.connect();
+      const sql =
+        'INSERT INTO orders (quantity, order_id, product_id) VALUES ($1, $2, $3)';
+      const result = await conn.query(sql, [quantity, order_id, product_id]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`unable to add product . Error: ${error}`);
+    }
+  }
+}
---
+++ src/models/product.ts
@@ -0,0 +1,48 @@
+import client from '../database';
+
+export type Product = {
+  id?: number;
+  name: string;
+  price: number;
+};
+
+export class ProductStore {
+  async index(): Promise<Product[]> {
+    try {
+      const conn = await client.connect();
+      const sql = 'SELECT * FROM products';
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (error) {
+      throw new Error(`unable to return the products. Error: ${error}`);
+    }
+  }
+
+  async show(id: number): Promise<Product> {
+    try {
+      const conn = await client.connect();
+      const sql = 'SELECT * FROM products WHERE id=($1)';
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(
+        `unable to return the product with id=${id}. Error: ${error}`
+      );
+    }
+  }
+
+  async create(p: Product): Promise<Product> {
+    try {
+      const conn = await client.connect();
+      const sql =
+        'INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *';
+      const result = await conn.query(sql, [p.name, p.price]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`unable to create the product`);
+    }
+  }
+}
